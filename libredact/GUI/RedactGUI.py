e2c6febdb24eee3ed799135848b9c68aa3ac373c
Gregory Jansen <gregory.jansen@gmail.com>
2016-08-29 19:32:39 -0400

    GUI now starts without virtualenv

--- libredact/GUI/RedactGUI.py
+++ libredact/GUI/RedactGUI.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # BitCurator Access Redaction Tool GUI (bca-redtools GUI)
@@ -13,25 +13,23 @@ import sys
 import time
 import libredact
 import threading
+import logging
+from libredact.redact import Redactor
+from libredact import config
 from subprocess import Popen, PIPE
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from RedactWindow import Ui_RedactWindow
 
 
-
 class RedactGUI(QtWidgets.QMainWindow, Ui_RedactWindow):
-	def __init__(self):
-		super(RedactGUI, self).__init__()
-		self.setupUi(self)
+    def __init__(self):
+        super(RedactGUI, self).__init__()
+        self.setupUi(self)
 
-		# ALL CONTROL CODE GOES HERE, IN SUBSEQUENT FUNCTIONS, IN ADDITIONAL MODULES, OR MAIN!
-		# *DO NOT* EDIT RedactWindow,py! (USE ONLY QTCREATOR TO UPDATE IT)
-        self.worker = RedactWorker()
-
-        # Read the redaction configuration file
-        config_path = self.SelectConfigEdit.getText()
-        cfg = libredact.config.parse(config_path)
+        # ALL CONTROL CODE GOES HERE, IN SUBSEQUENT FUNCTIONS, IN ADDITIONAL MODULES, OR MAIN!
+        # *DO NOT* EDIT RedactWindow,py! (USE ONLY QTCREATOR TO UPDATE IT)
+        # self.worker = RedactWorker()
 
         # Override any CLI arguments
         # if args.get('--input'):
@@ -46,55 +44,52 @@ class RedactGUI(QtWidgets.QMainWindow, Ui_RedactWindow):
         #     cfg['commit'] = False
         # cfg['detail'] = args.get('--detail')
 
-        logging.debug('Combined config & arguments:\n%s' % cfg)
-        redactor = Redactor(**cfg)
-
         # Custom signal connection to update redaction progress bar
-        self.connect(self.thread, SIGNAL("progress(int, int)"), self.updateProgressBar)
+        # RedactWindow.connect(self.thread, SIGNAL("progress(int, int)"), self.updateProgressBar)
+        # Handle buttons in main tab - this may be better done elsewhere
+        self.CloseButton.clicked.connect(self.buttonClickedClose)
+        self.CancelButton.clicked.connect(self.buttonClickedCancel)
+        self.CancelButton.clicked.connect(self.buttonClickedRun)
 
-		# Handle buttons in main tab - this may be better done elsewhere
-		self.CloseButton.clicked.connect(self.buttonClickedClose)
-		self.CancelButton.clicked.connect(self.buttonClickedCancel)
-		self.CancelButton.clicked.connect(self.buttonClickedRun)
-
-		# Handle button presses in config edit tab - this may be better done elsewhere
-		self.LoadButton.clicked.connect(self.buttonClickedLoad)
-		self.SaveButton.clicked.connect(self.buttonClickedSave)
+        # Handle button presses in config edit tab - this may be better done elsewhere
+        self.LoadButton.clicked.connect(self.buttonClickedLoad)
+        self.SaveButton.clicked.connect(self.buttonClickedSave)
 
     def updateProgressBar(self, offset, total):
         fraction = offset/total
         self.RedactionProgress.setProperty("value", fraction)
 
-	def buttonClickedClose(self):
-		# Quit the app, duh
-		QtCore.QCoreApplication.instance().quit()
+    def buttonClickedClose(self):
+        # Quit the app, duh
+        QtCore.QCoreApplication.instance().quit()
 
-	def buttonClickedCancel(self):
-		# Placeholder - fix for actual app
-		QtCore.QCoreApplication.instance().quit()
+    def buttonClickedCancel(self):
+        # Placeholder - fix for actual app
+        QtCore.QCoreApplication.instance().quit()
 
-	def buttonClickedRun(self):
-		# Placeholder - fix for actual app
-		self.worker.redact(redactor)
+    def buttonClickedRun(self):
+        # Placeholder - fix for actual app
+        self.worker.redact(self.redactor)
 
-	def buttonClickedLoad(self):
-		# Placeholder - fix for actual app
-		QtCore.QCoreApplication.instance().quit()
+    def buttonClickedLoad(self):
+        # Read the redaction configuration file
+        config_path = self.SelectConfigEdit.text()
+        cfg = config.parse(config_path)
+        logging.debug('Combined config & arguments:\n%s' % cfg)
+        # validate the config against schema and show any errors
+        self.redactor = Redactor(**cfg)
 
-	def buttonClickedSave(self):
-		# Placeholder - fix for actual app
-		QtCore.QCoreApplication.instance().quit()
-
+    def buttonClickedSave(self):
+        # Placeholder - fix for actual app
+        QtCore.QCoreApplication.instance().quit()
 
 
 if __name__ == '__main__':
 
-	import sys
+    app = QtWidgets.QApplication(sys.argv)
 
-	app = QtWidgets.QApplication(sys.argv)
+    # UI setup is performed in the RedactGUI class
+    form = RedactGUI()
+    form.show()
 
-	# UI setup is performed in the RedactGUI class
-	form = RedactGUI()
-	form.show()
-
-	sys.exit(app.exec_())
+    sys.exit(app.exec_())
