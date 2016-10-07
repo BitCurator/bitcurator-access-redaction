67f3532374b03b263f2df7a5ce9c6db51b12f6e7
Gregory Jansen <gregory.jansen@gmail.com>
2016-10-07 12:38:50 +0200

    specify python 2.7, not 3

--- libredact/setup.py
+++ libredact/setup.py
@@ -44,10 +44,7 @@ setup(name='libredact',
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
                    "Programming Language :: Python",
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.0'
-                   "Programming Language :: Python :: 3.1",
-                   "Programming Language :: Python :: 3.2",
+                   "Programming Language :: Python :: 2.7",
                    "Operating System :: OS Independent",
                    "Topic :: Software Development :: Libraries :: Python Modules"],
       keywords="bitcurator")
